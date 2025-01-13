73985b7e018a0c00e5855f20e5cbe8a9d16e99bd
yugandhar0609 <yugandharselvam@gmail.com>
2025-01-13 14:06:04 +0530

    changes

--- Jee_frontend/src/pages/Subscription.jsx
+++ Jee_frontend/src/pages/Subscription.jsx
@@ -393,7 +393,7 @@ const Subscription = () => {
           <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
           </svg>
-          Back
+          Back to Dashboard
         </button>
 
         {isSubscribed && currentPlan ? (
