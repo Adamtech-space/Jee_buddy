76339c4a0bea965d8b5f886dca7e59bb2d5a126b
yugandhar0609 <yugandharselvam@gmail.com>
2025-01-21 11:10:02 +0530

    Update axios interceptors to switch base URLs for production deployment
    - Changed base URL in 'aiAxios.js' from 'https://jee-buddy-ai-ten.vercel.app/' to 'https://jee-buddy-ai.vercel.app/'.
    - Updated base URL in 'axios.jsx' to 'https://jee-buddy-backend.vercel.app/v1' for consistent API integration.
    - Commented out local development URLs to streamline configuration.

--- Jee_frontend/src/interceptors/aiAxios.js
+++ Jee_frontend/src/interceptors/aiAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const aiInstance = axios.create({
   // baseURL: 'http://127.0.0.1:8000/',
-  baseURL: 'https://jee-buddy-ai-ten.vercel.app/',
+  baseURL: 'https://jee-buddy-ai.vercel.app/',
   headers: {
     'Content-Type': 'application/json',
   },
