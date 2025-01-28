7e58c979a7449cec15b74c8d3be14a146d1e6a66
MrJayasuriya <jayasuriyathedatascientst@gmail.com>
2025-01-28 10:58:56 +0530

    feat: add WebSocket support for real-time communication
    - Integrated Django Channels for WebSocket functionality
    - Updated requirements.txt with necessary WebSocket dependencies
    - Configured ASGI application to support HTTP and WebSocket protocols
    - Added Channels to Django installed apps and configured channel layers
    - Prepared infrastructure for real-time features in the backend

--- Jee_buddy_ai_backend/src/core/settings.py
+++ Jee_buddy_ai_backend/src/core/settings.py
@@ -135,6 +135,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
+INSTALLED_APPS += ['channels']
+
+ASGI_APPLICATION = 'core.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
