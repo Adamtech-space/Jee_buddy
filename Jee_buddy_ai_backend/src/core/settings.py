4df0bea2bce10b6eef3a543499d50f071f95fb71
yugandhar0609 <yugandharselvam@gmail.com>
2025-02-03 22:29:21 +0530

    chore: Update CORS settings for enhanced security
    - Disabled CORS_ALLOW_ALL_ORIGINS
    - Disabled CORS_ALLOW_CREDENTIALS
    - Improved backend security configuration

--- Jee_buddy_ai_backend/src/core/settings.py
+++ Jee_buddy_ai_backend/src/core/settings.py
@@ -140,6 +140,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
