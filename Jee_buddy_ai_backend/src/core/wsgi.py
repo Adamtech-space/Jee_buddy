71815b6a313a6623076b2601294400667c20d0f1
yugandhar0609 <yugandharselvam@gmail.com>
2025-02-05 18:52:21 +0530

    changes

--- Jee_buddy_ai_backend/src/core/wsgi.py
+++ Jee_buddy_ai_backend/src/core/wsgi.py
@@ -15,6 +15,6 @@ from django.core.wsgi import get_wsgi_application
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.core.settings')
 
 application = get_wsgi_application()
